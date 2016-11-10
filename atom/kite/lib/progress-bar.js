8f4d229ea1068368099c12ad074019a17ef9f670
Daniel Hung <daniel@kite.com>
2016-11-09 16:51:45 -0800

    add less variables; more progress bar functionality

--- atom/kite/lib/progress-bar.js
+++ atom/kite/lib/progress-bar.js
@@ -14,10 +14,25 @@ var ProgressBar = class {
     this.percentage.classList.add('progress-bar-percentage');
     this.setPercentage();
     this.element.appendChild(this.percentage);
+
+    this.timerID = null;
   }
 
   setPercentage() {
+    if (this.progress === 100) {
+      this.percentage.classList.add('finished');
+    } else {
+      this.percentage.classList.remove('finished');
+    }
     this.percentage.style.width = this.progress + '%';
+  }
+
+  increment(delta) {
+    if (this.progress <= 0 || this.progress >= 100) {
+      return;
+    }
+    this.progress = Math.max(0, Math.min(100, this.progress + delta));
+    this.setPercentage();
   }
 };
 
