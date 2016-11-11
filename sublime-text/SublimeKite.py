55b3936fad7be6af3fdb14fcc448920ffaf956e5
Oscar Campos <oscar.campos@member.fsf.org>
2016-11-11 21:29:07 +0000

    simple asynchronous implementation of completion queries for sublime_text plugin

--- sublime-text/SublimeKite.py
+++ sublime-text/SublimeKite.py
@@ -13,9 +13,13 @@ import sublime_plugin
 PYTHON3 = sys.version_info >= (3,)
 
 if PYTHON3:
+    import http.client
     from queue import Queue, Full
+    defer = sublime.set_timeout_async
 else:
+    import urllib2
     from Queue import Queue, Full
+    defer = sublime.set_timeout
 
 FIX_APPLI_ERROR = (
     'It is with great regret we must inform you that we cannot apply the '
@@ -31,14 +35,18 @@ KITED_HOSTPORT = "127.0.0.1:46624"
 EVENT_ENDPOINT = "/clientapi/editor/event"
 ERROR_ENDPOINT = "/clientapi/editor/error"
 COMPLETIONS_ENDPOINT = "/clientapi/editor/completions"
-HTTP_TIMEOUT = 0.09  # timeout for HTTP requests in seconds
+HTTP_TIMEOUT = 0.50  # timeout for HTTP requests in seconds
 EVENT_QUEUE_SIZE = 3  # small queue because we want to throw away old events
 
 VERBOSE = False
-ENABLE_COMPLETIONS = False
+ENABLE_COMPLETIONS = True
 
 
 class SublimeKite(sublime_plugin.EventListener):
+
+    _completions = []
+    _ready_from_kite_server = False
+
     def __init__(self):
         self._event_queue = Queue(maxsize=EVENT_QUEUE_SIZE)
         self._event_thread = threading.Thread(target=self._event_loop)
@@ -75,38 +83,72 @@ class SublimeKite(sublime_plugin.EventListener):
         """
         on_query_completions is called when sublime is about to show completions  # noqa
         """
-        if not ENABLE_COMPLETIONS:
+
+        if not ENABLE_COMPLETIONS or not is_python_buffer(view):
             return
 
-        # do not attempt multi-location completions for now
-        if len(locations) != 1:
-            verbose("ignoring request for completions with %d locations" % len(
-                locations)
-            )
-            return
+        # check if we have already a result from the kite server
+        if self._ready_from_kite_server:
+            completions = self._completions
 
-        resp = self._http_roundtrip(COMPLETIONS_ENDPOINT, {
-            "source": SOURCE,
-            "filename": realpath(view.file_name()),
-            "text": view.substr(sublime.Region(0, view.size())),
-            "cursor": locations[0],
-        })
-        verbose("completions response:", resp)
+            cpl = []
+            for c in completions:
+                cpl.append(('{0}\t{1}'.format(
+                    c.get('display', ''),
+                    c.get('hint', 'kite')
+                ), c.get('insert', '')))
+
+            verbose('returning completions:', cpl)
+            self._completions = []
+            self._ready_from_kite_server = False
+            return (cpl, 0 | sublime.INHIBIT_WORD_COMPLETIONS)
+
+        # if there is more than one location, ignore all but the first one
+        location = locations[0]
+        defer(lambda: self._fire_completion(view, location, self._complete), 0)
+
+    def _fire_completion(self, view, location, callback):
+        """Fire the completion, regiuster the callback and wait for it
+        """
+
+        resp = self._http_roundtrip(
+            COMPLETIONS_ENDPOINT, {
+                'source': SOURCE,
+                'filename': realpath(view.file_name()),
+                'text': view.substr(sublime.Region(0, view.size())),
+                'cursor': location
+            }
+        )
+        verbose('completions response:', resp)
+        callback(resp)
+
+    def _complete(self, resp):
+        """At this point we have a response from Kite and we inject it
+        """
+
         if resp is None:
             return
 
-        completions = resp.get("completions", None)
-        if completions is None:
-            return
+        completions = resp.get('completions')
+        if completions is not None:
+            self._completions = completions
 
-        out = []
-        for c in completions:
-            display = c.get("display", "")
-            insert = c.get("insert", "")
-            hint = c.get("hint", "kite")
-            out.append(("%s\t%s" % (display, hint), insert))
-        verbose("returning completions:", out)
-        return out
+        if self._completions:
+            view = sublime.active_window().active_view()
+            view.run_command('hide_auto_complete')
+            self._ready_from_kite_server = True
+            self._run_auto_complete()
+
+    def _run_auto_complete(self):
+        """call autocomplete trough ST API
+        """
+
+        sublime.active_window().active_view().run_command('auto_complete', {
+            'disable_auto_insert': True,
+            'api_completions_only': False,
+            'next_completion_if_showing': False,
+            'auto_complete_commit_on_tab': True,
+        })
 
     def _update(self, action, view):
         # Check view group and index to determine if in source code buffer
@@ -162,7 +204,6 @@ class SublimeKite(sublime_plugin.EventListener):
             req = json.dumps(payload)
 
             if PYTHON3:
-                import http.client
                 conn = http.client.HTTPConnection(
                     KITED_HOSTPORT, timeout=HTTP_TIMEOUT
                 )
@@ -171,7 +212,6 @@ class SublimeKite(sublime_plugin.EventListener):
                 resp = response.read().decode('utf-8')
                 conn.close()
             else:
-                import urllib2
                 url = "http://" + KITED_HOSTPORT + endpoint
                 conn = urllib2.urlopen(url, data=req, timeout=HTTP_TIMEOUT)
                 resp = conn.read()
@@ -201,3 +241,19 @@ def verbose(*args):
     """
     if VERBOSE:
         print(*args)
+
+
+def is_python_buffer(view):
+    """determine if the active buffer is Python code
+    """
+
+    if view is None:
+        return False
+
+    try:
+        location = view.sel()[0].begin()
+    except IndexError:
+        return False
+
+    # ignore comments
+    return view.match_selector(location, 'source.python - comment')
